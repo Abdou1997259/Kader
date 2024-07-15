10c310375f226556502b9e3ec9e6a58e8f09e44a
Mahmoud-ibrahim74 <mhmoud.ibrahim74@gmail.com>
2024-07-15 11:15:08 +0300

    fix IRequestService ambguios

--- Kader_System.Api/Areas/HR/Controllers/ContractsController.cs
+++ Kader_System.Api/Areas/HR/Controllers/ContractsController.cs
@@ -7,7 +7,7 @@ namespace Kader_System.Api.Areas.HR.Controllers
     [ApiController]
     [Authorize(Permissions.HR.View)]
     [Route("api/v1/")]
-    public class ContractsController(IContractService contractService, Services.IServices.HTTP.ITitleService requestService) : ControllerBase
+    public class ContractsController(IContractService contractService, IRequestService requestService) : ControllerBase
     {
         private readonly Services.IServices.HTTP.ITitleService requestService = requestService;
 
