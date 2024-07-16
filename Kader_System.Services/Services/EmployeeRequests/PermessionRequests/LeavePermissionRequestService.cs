039c8fafe3f48e6683610a8dea7eb26854bda88f
Mahmoud-ibrahim74 <mhmoud.ibrahim74@gmail.com>
2024-07-16 13:53:02 +0300

    add new folders of Employee Request

--- Kader_System.Services/Services/EmployeeRequests/PermessionRequests/LeavePermissionRequestService.cs
+++ Kader_System.Services/Services/EmployeeRequests/PermessionRequests/LeavePermissionRequestService.cs
@@ -6,13 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kader_System.Services.IServices.EmployeeRequests.Requests
+namespace Kader_System.Services.Services.EmployeeRequests.PermessionRequests
 {
     public class LeavePermissionRequestService : ILeavePermissionRequestService
     {
         public Task<List<DTOLeavePermissionRequest>> GetAllLeavePermissionRequests()
         {
-             
+
         }
         public Task<int> AddNewLeavePermissionRequest(DTOLeavePermissionRequest model) { }
         public Task<int> UpdateLeavePermissionRequest(DTOLeavePermissionRequest model) { }
