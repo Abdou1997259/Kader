9ea92175b290442ce39e523a1da2d9f48d3d5ce5
Ali780909 <proali1997@gmail.com>
2024-07-16 14:21:56 +0300

    leave

---
+++ Kader_System.Services/Services/EmployeeRequests/PermessionRequests/LeavePermissionRequestService.cs
@@ -0,0 +1,21 @@
+﻿using Kader_System.Domain.DTOs.Request.EmployeesRequests.PermessionRequests;
+using Kader_System.Services.IServices.EmployeeRequests.PermessionRequests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kader_System.Services.Services.EmployeeRequests.PermessionRequests
+{
+    public class LeavePermissionRequestService : ILeavePermissionRequestService
+    {
+        public Task<List<DTOLeavePermissionRequest>> GetAllLeavePermissionRequests()
+        {
+
+        }
+        public Task<int> AddNewLeavePermissionRequest(DTOLeavePermissionRequest model) { }
+        public Task<int> UpdateLeavePermissionRequest(DTOLeavePermissionRequest model) { }
+        public Task<int> DeleteLeavePermissionRequest(int id) { }
+    }
+}
