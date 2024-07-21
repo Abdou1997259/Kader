d02f0af5114428344bf3a222be2e7dec2d720c46
Ali780909 <proali1997@gmail.com>
2024-07-21 15:24:08 +0300

    After loanRequestRepo

--- Kader_System.DataAccess/Repositories/EmployeeRequests/Requests/loanRequestRepository.cs
+++ Kader_System.DataAccess/Repositories/EmployeeRequests/Requests/loanRequestRepository.cs
@@ -1,14 +1,9 @@
-﻿using Kader_System.Domain.Interfaces.EmployeeRequest.Request;
+﻿
+using Kader_System.Domain.Interfaces.EmployeeRequest.Request;
 using Kader_System.Domain.Models.EmployeeRequests.Requests;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kader_System.DataAccess.Repositories.EmployeeRequests.Requests
 {
-    public  class loanRequestRepository (KaderDbContext context) : BaseRepository<LoanRequest>(context) , ILoanRequesServicesRepository
-    {
+    public class LoanRequestRepository(KaderDbContext db) : BaseRepository<LoanRequest>(db), ILoanRequestRepository { 
     }
 }
