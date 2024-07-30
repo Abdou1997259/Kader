7fbaa543693b3f4735f49b72db0253bf6579bcf7
Ali780909 <proali1997@gmail.com>
2024-07-30 16:50:56 +0300

    AFter Edite TreeSCreen Done

--- Kader_System.Domain/Models/Setting/StScreenSub.cs
+++ Kader_System.Domain/Models/Setting/StScreenSub.cs
@@ -17,8 +17,6 @@ public class StScreenSub : BaseEntity
     [ForeignKey(nameof(ScreenCatId))]
     public StMainScreenCat ScreenCat { get; set; } = default!;
 
-    public ICollection<StScreenSub> ScreenSubs { get; set; }
-
 
     public ICollection<StSubMainScreenAction> ListOfActions { get; set; } = [];
 }
