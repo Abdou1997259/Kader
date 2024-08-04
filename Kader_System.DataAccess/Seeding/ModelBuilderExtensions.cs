278afdc1be746cc400b64a70f76a463e0de9ca23
Mahmoud-ibrahim74 <mhmoud.ibrahim74@gmail.com>
2024-08-04 17:04:11 +0300

    add builder_st_sub_main_screen_actions

--- Kader_System.DataAccess/Seeding/ModelBuilderExtensions.cs
+++ Kader_System.DataAccess/Seeding/ModelBuilderExtensions.cs
@@ -278,6 +278,15 @@ new StScreenSub { Id = 27, ScreenCatId = 4, Screen_sub_title_ar = "زيادة ا
 
                  );
 
+        modelBuilder.Entity<StSubMainScreenAction>()
+                 .HasData(
+                    new StSubMainScreenAction() { Id = 1, ScreenSubId = 4, ActionId = 1 },
+                    new StSubMainScreenAction() { Id = 2, ScreenSubId = 1, ActionId = 3 },
+                    new StSubMainScreenAction() { Id = 4, ScreenSubId = 4, ActionId = 2 },
+                    new StSubMainScreenAction() { Id = 5, ScreenSubId = 1, ActionId = 1 },
+                    new StSubMainScreenAction() { Id = 9, ScreenSubId = 2, ActionId = 2 }
+                 );
+
 
 
 
