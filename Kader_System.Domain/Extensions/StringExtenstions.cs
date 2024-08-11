268309f9345fb1e31ef3117122d87937fcba4ca6
Mahmoud-ibrahim74 <mhmoud.ibrahim74@gmail.com>
2024-08-11 15:22:01 +0300

    edit userPermession

--- Kader_System.Domain/Extensions/StringExtenstions.cs
+++ Kader_System.Domain/Extensions/StringExtenstions.cs
@@ -1,11 +1,13 @@
 ﻿
 
- namespace Kader_System.Domain.Extensions
+using Kader_System.Domain.DTOs.Response;
+
+namespace Kader_System.Domain.Extensions
 {
     public static class StringExtenstions
     {
 
-       
+
         public static List<int> Splitter(this string str)
         {
             if (string.IsNullOrWhiteSpace(str))
@@ -41,17 +43,42 @@
             return string.Join(',', intergers);
 
         }
-            public static string NulalbleConcater(this List<int?> intergers)
+        public static string NulalbleConcater(this List<int?> intergers)
+        {
+
+
+            if (intergers is null)
             {
-
-
-                if (intergers is null)
-                {
-                    return "";
-                }
-                return string.Join(',', intergers);
-
+                return "";
             }
+            return string.Join(',', intergers);
 
         }
+        public static Dictionary<string, bool> CreateNewPermission(this string res, string mainActions, string userActions, string permNames)
+        {
+            // Split the input strings into arrays
+            var mainActionsArr = mainActions.Split(',').ToArray(); // [1,2,3,4]
+            var userActionsArr = userActions.Split(',').ToArray(); // [2,3]
+            var permNamesArr = permNames.Split(',').ToArray(); // [view,add,edit,delete]
+
+            // Create the action-to-permission map using LINQ
+            var actionToPermNameMap = mainActionsArr
+                .Select((actionId, index) => new { actionId, permName = permNamesArr[index] })
+                .ToDictionary(x => x.actionId, x => x.permName);
+
+            // Create the result dictionary using LINQ
+            var result = permNamesArr
+                .ToDictionary(
+                    permName => permName,
+                    permName => actionToPermNameMap
+                        .FirstOrDefault(x => x.Value == permName).Key != null
+                        && userActionsArr.Contains(actionToPermNameMap.FirstOrDefault(x => x.Value == permName).Key)
+                );
+
+            return result;
+
+        }
+
+
+    }
 }
