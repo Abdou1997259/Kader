0eec8642f3453439aea6499f50bf622ad09e96fb
Ali780909 <proali1997@gmail.com>
2024-08-11 17:00:27 +0300

    Add GetAllScreens

---
+++ Kader_System.Services/IServices/Setting/IGetAllScreensService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kader_System.Services.IServices.Setting
+{
+    public interface IGetAllScreensService
+    {
+        public Task<Response<Dictionary<string, List<Dictionary<string, object>>>>> GetAllScreens(string lang);
+
+    }
+}
