a51cfb6bb6e2d4be4f27b7c93f46a9df3cbe75a8
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-08-12 22:57:06 +0300

    mohamed abdou

--- Kader_System.Domain/DTOs/Response/CreateUserResponse.cs
+++ Kader_System.Domain/DTOs/Response/CreateUserResponse.cs
@@ -12,8 +12,8 @@ namespace Kader_System.Domain.DTOs.Response
       
         public string Email { get; set; }
         public string FullName { get; set; }
-        public List<int> TitleId { get; set; }
-        public List<int> CompanyId { get; set; }
+        public List<int?> TitleId { get; set; }
+        public List<int?> CompanyId { get; set; }
         public int JobTitle { get; set; }
         public int CompanyYear { get; set; }
         public string Token { get; set; }
