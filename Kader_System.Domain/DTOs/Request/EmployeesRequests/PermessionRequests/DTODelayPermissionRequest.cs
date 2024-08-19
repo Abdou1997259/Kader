f20ee3bcd57dab54e135295ae225a6879fd65be1
Mahmoud-ibrahim74 <mhmoud.ibrahim74@gmail.com>
2024-08-19 12:08:47 +0300

    edit DelayHours in DelayPermissionRequest

--- Kader_System.Domain/DTOs/Request/EmployeesRequests/PermessionRequests/DTODelayPermissionRequest.cs
+++ Kader_System.Domain/DTOs/Request/EmployeesRequests/PermessionRequests/DTODelayPermissionRequest.cs
@@ -11,7 +11,7 @@ namespace Kader_System.Domain.DTOs.Request.EmployeesRequests.PermessionRequests
         [Required(ErrorMessage = "please Insert EmployeeId")]
         public int EmployeeId { get; set; }
         [Required(ErrorMessage = "please Insert HoursDelay")]
-        public int HoursDelay { get; set; }
+        public int DelayHours { get; set; }
         public string? Notes { get; set; }
         [AllowedLetters(FileSettings.SpecialChar), MaxFileLettersCount(FileSettings.Length), FileExtensionValidation(FileSettings.AllowedExtension)]
         public IFormFile? Attachment { get; set; }
