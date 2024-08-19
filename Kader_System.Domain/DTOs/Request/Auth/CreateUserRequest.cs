dda32e37324e242bde92044157b9d150332c65f1
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-08-19 10:37:55 +0300

    ....

--- Kader_System.Domain/DTOs/Request/Auth/CreateUserRequest.cs
+++ Kader_System.Domain/DTOs/Request/Auth/CreateUserRequest.cs
@@ -16,8 +16,8 @@ namespace Kader_System.Domain.DTOs.Request.Auth
         public string phone { get; set; }
         public string email { get; set; }
         public string full_name { get; set; }
-        public int? current_title { get; set; } = 1;
-        public int? current_company { get; set; } = 3;
+        public int? current_title { get; set; } 
+        public int? current_company { get; set; }
         public List<int?> title_id {  get; set; }=new List<int?>() { };
         public List<int?> company_id { get; set; } = new List<int?>() { };
         public  int job_title { get; set; }
