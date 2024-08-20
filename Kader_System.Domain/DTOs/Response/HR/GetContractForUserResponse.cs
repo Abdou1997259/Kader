7ac7de4efbaf9277d96dfdb940e5f55f9bfdfc74
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-08-20 15:52:04 +0300

    ....

--- Kader_System.Domain/DTOs/Response/HR/GetContractForUserResponse.cs
+++ Kader_System.Domain/DTOs/Response/HR/GetContractForUserResponse.cs
@@ -4,16 +4,22 @@ namespace Kader_System.Domain.DTOs.Response.HR
 {
     public class GetContractForUserResponse
     {
-        public double SalaryFixed { get; set; }
-        public double SalaryTotal { get; set; }   
-        public string AddedBy { get; set; }
-        public string ContractFile { get; set; }    
-        public double HousingAllowance { get; set; }
         public int Id { get; set; }
-        public bool Active { get; set; }
-        public DateOnly StartDate { get; set; } 
-        public string EmployeeName { get; set; }
-        public DateOnly EndDate { get; set; }   
+        public List<Items> Items { get; set; }
+       
 
+    }
+    public class Items
+    {
+        public double SalaryFixed { get; set; }
+        public double SalaryTotal { get; set; }
+        public string AddedBy { get; set; }
+        public string ContractFile { get; set; }
+        public double HousingAllowance { get; set; }
+        
+        public bool Active { get; set; }
+        public DateOnly StartDate { get; set; }
+        public string EmployeeName { get; set; }
+        public DateOnly EndDate { get; set; }
     }
 }
