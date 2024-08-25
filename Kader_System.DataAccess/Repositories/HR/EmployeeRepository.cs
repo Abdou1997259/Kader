8fb814245d5ee953e292f8f5a293a6a35602dc67
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-08-25 15:11:03 +0300

    ...

--- Kader_System.DataAccess/Repositories/HR/EmployeeRepository.cs
+++ Kader_System.DataAccess/Repositories/HR/EmployeeRepository.cs
@@ -115,7 +115,8 @@ public class EmployeeRepository(KaderDbContext context) : BaseRepository<HrEmplo
                              shift_name = lang == Localization.Arabic ? sh.Name_ar : sh.Name_en,
                              allowances_sum = employeeAllowances.Any() ? employeeAllowances.AsEnumerable().Sum(a => a.Amount) : 0,
                              employee_loans_sum = 0,
-                             employee_attachments=employee.ListOfAttachments.Select(s=>new EmployeeAttachmentForEmp
+                            title_id= usr.CurrentTitleId,
+                             employee_attachments =employee.ListOfAttachments.Select(s=>new EmployeeAttachmentForEmp
                              {
                                  FileName=s.FileName,
                                  Extention=s.FileExtension,
