e7f9a81370b146ccdc132fb21b0752d772339cee
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-09-29 15:01:54 +0300

    resolve some issues

--- Kader_System.DataAccess/Repositories/InterviewRepositories/ApplicantRepository.cs
+++ Kader_System.DataAccess/Repositories/InterviewRepositories/ApplicantRepository.cs
@@ -1,4 +1,5 @@
-﻿using Kader_System.Domain.Models.Interviews;
+﻿using Kader_System.Domain.Interfaces.EducationRepositories;
+using Kader_System.Domain.Models.Interviews;
 
 namespace Kader_System.DataAccess.Repositories.InterviewRepositories
 {
